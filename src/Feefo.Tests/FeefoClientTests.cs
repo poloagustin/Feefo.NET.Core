70808202ab1c54ec8b4a9f5c7850bdafd9acc0a4
kevbite <kev_bite@msn.com>
2015-09-20 18:19:59 +0100

    More code coverage.

--- src/Feefo.Tests/FeefoClientTests.cs
+++ src/Feefo.Tests/FeefoClientTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Policy;
+using Feefo.Responses;
 using NUnit.Framework;
 
 namespace Feefo.Tests
@@ -17,10 +18,11 @@ namespace Feefo.Tests
         public void GivenAFeefoClientWithAFakeClientHandler()
         {
             var baseUri = "http://www.feefo.com/feefo/xmlfeed.jsp";
-            var feefoSettings = new FeefoSettings() {BaseUri = new Uri(baseUri), Logon = "www.examplesupplier.com" };
+            var feefoSettings = new FeefoSettings() {BaseUri = new Uri(baseUri), Logon = "www.examplesupplier.com"};
 
-            _httpMessageHandler = new StubHttpMessageHandler(new Uri($"{baseUri}?logon={feefoSettings.Logon}&json=true"),
-                                            new ResourceHelper().GetStringResource("Feefo.Tests.FeefoRssFeed.json"));
+            _httpMessageHandler = new StubHttpMessageHandler(
+                new Uri($"{baseUri}?logon={feefoSettings.Logon}&json=true"),
+                new ResourceHelper().GetStringResource("Feefo.Tests.FeefoRssFeed.json"));
             _client = new FeefoClient(_httpMessageHandler, feefoSettings);
         }
 
@@ -48,7 +50,8 @@ namespace Feefo.Tests
             Assert.That(_result.FeedbackList.SUMMARY.SERVICEGOOD, Is.EqualTo(1084));
             Assert.That(_result.FeedbackList.SUMMARY.SERVICEPOOR, Is.EqualTo(203));
             Assert.That(_result.FeedbackList.SUMMARY.START, Is.EqualTo(1));
-            Assert.That(_result.FeedbackList.SUMMARY.SUPPLIERLOGO, Is.EqualTo("http://www.feefo.com/feefo/getnonpublicfile.jsp?vendorimage=13&vendorimageext=file.png"));
+            Assert.That(_result.FeedbackList.SUMMARY.SUPPLIERLOGO,
+                Is.EqualTo("http://www.feefo.com/feefo/getnonpublicfile.jsp?vendorimage=13&vendorimageext=file.png"));
             Assert.That(_result.FeedbackList.SUMMARY.TITLE, Is.EqualTo("Example Supplier Ltd."));
             Assert.That(_result.FeedbackList.SUMMARY.TOTALPRODUCTCOUNT, Is.EqualTo(2890));
             Assert.That(_result.FeedbackList.SUMMARY.TOTALRESPONSES, Is.EqualTo(3871));
@@ -57,32 +60,186 @@ namespace Feefo.Tests
             Assert.That(_result.FeedbackList.SUMMARY.VendorRef, Is.Not.Null);
             Assert.That(_result.FeedbackList.SUMMARY.WORST, Is.EqualTo(0));
         }
-
+        
         [Test]
-        public void ThenTheResultContainsTheCorrectCounts()
+        public void ThenTheResultContainsTheCorrectAmountOfFeedback()
         {
-            Assert.That(_result.FeedbackList.FEEDBACK.Count, Is.EqualTo(20));
+            var expectedCount = GetExpectedFeedback().Count;
+
+            Assert.That(_result.FeedbackList.FEEDBACK.Count, Is.EqualTo(expectedCount));
         }
 
         [Test]
-        public void ThenTheFeedbackId7445488IsCorrect()
+        public void ThenTheFeedbackIsCorrect()
         {
-            var feedback = _result.FeedbackList.FEEDBACK.First(x => x.FEEDBACKID == 7445488);
+            var expectedFeedback = GetExpectedFeedback();
 
-            Assert.That(feedback.COUNT, Is.EqualTo(1));
-            Assert.That(feedback.CUSTOMERCOMMENT, Is.EqualTo("Service rating : Really good service<br/>Product : Good camera for the price."));
-            Assert.That(feedback.DATE, Is.EqualTo("10-Sep-2015"));
-            Assert.That(feedback.DESCRIPTION, Is.EqualTo("Konica Minolta Z20"));
-            Assert.That(feedback.FACEBOOKSHARELINK, Is.EqualTo("http://www.facebook.com/sharer.php?u=http%3A%2F%2Fwww.feefo.com%2FGB%2Fen%2Freviews%2FExample-Supplier-Ltd%2F%3Fid%3D13%26servicefeedbackid%3D7445488"));
-            Assert.That(feedback.FEEDBACKID, Is.EqualTo(7445488));
-            Assert.That(feedback.HREVIEWDATE, Is.EqualTo(new DateTime(2015, 09, 10, 11, 25, 58)));
-            Assert.That(feedback.HREVIEWRATING, Is.EqualTo(4));
-            Assert.That(feedback.LINK, Is.EqualTo("http://www.examplesupplier.com/product_info.php?products_id=1333OSCommerceOSCommerceOSCommerce"));
-            Assert.That(feedback.PRODUCTCODE, Is.EqualTo(133));
-            Assert.That(feedback.PRODUCTRATING, Is.EqualTo("+"));
-            Assert.That(feedback.READMOREURL, Is.EqualTo("http://www.feefo.com/GB/en/reviews/Example-Supplier-Ltd/?id=13&servicefeedbackid=7445488"));
-            Assert.That(feedback.SERVICERATING, Is.EqualTo("++"));
-            Assert.That(feedback.SHORTCUSTOMERCOMMENT, Is.EqualTo("Service rating : Really good service<br/>Product : Good camera for the price."));
+            foreach(var feedback in _result.FeedbackList.FEEDBACK)
+            {
+                var expected = expectedFeedback.First(x => x.FEEDBACKID == feedback.FEEDBACKID);
+
+                Assert.That(feedback.COUNT, Is.EqualTo(expected.COUNT));
+                Assert.That(feedback.CUSTOMERCOMMENT, Is.EqualTo(expected.CUSTOMERCOMMENT));
+                Assert.That(feedback.DATE, Is.EqualTo(expected.DATE));
+                Assert.That(feedback.DESCRIPTION, Is.EqualTo(expected.DESCRIPTION));
+                Assert.That(feedback.FACEBOOKSHARELINK, Is.EqualTo(expected.FACEBOOKSHARELINK));
+                Assert.That(feedback.HREVIEWDATE, Is.EqualTo(expected.HREVIEWDATE));
+                Assert.That(feedback.HREVIEWRATING, Is.EqualTo(expected.HREVIEWRATING));
+                Assert.That(feedback.LINK, Is.EqualTo(expected.LINK));
+                Assert.That(feedback.PRODUCTCODE, Is.EqualTo(expected.PRODUCTCODE));
+                Assert.That(feedback.PRODUCTRATING, Is.EqualTo(expected.PRODUCTRATING));
+                Assert.That(feedback.READMOREURL, Is.EqualTo(expected.READMOREURL));
+                Assert.That(feedback.SERVICERATING, Is.EqualTo(expected.SERVICERATING));
+                Assert.That(feedback.SHORTCUSTOMERCOMMENT, Is.EqualTo(expected.SHORTCUSTOMERCOMMENT));
+            }
+        }
+
+        public List<Feedback> GetExpectedFeedback()
+        {
+            return new List<Feedback>()
+            {
+                new Feedback
+                {
+                    COUNT = 1,
+                    CUSTOMERCOMMENT = "Service rating : Really good service<br/>Product : Good camera for the price.",
+                    DATE = "10-Sep-2015",
+                    DESCRIPTION = "Konica Minolta Z20",
+                    FACEBOOKSHARELINK =
+                        "http://www.facebook.com/sharer.php?u=http%3A%2F%2Fwww.feefo.com%2FGB%2Fen%2Freviews%2FExample-Supplier-Ltd%2F%3Fid%3D13%26servicefeedbackid%3D7445488",
+                    FEEDBACKID = 7445488,
+                    HREVIEWDATE = new DateTime(2015, 09, 10, 11, 25, 58),
+                    HREVIEWRATING = 4,
+                    LINK =
+                        "http://www.examplesupplier.com/product_info.php?products_id=1333OSCommerceOSCommerceOSCommerce",
+                    PRODUCTCODE = 133,
+                    PRODUCTRATING = "+",
+                    READMOREURL =
+                        "http://www.feefo.com/GB/en/reviews/Example-Supplier-Ltd/?id=13&servicefeedbackid=7445488",
+                    SERVICERATING = "++",
+                    SHORTCUSTOMERCOMMENT =
+                        "Service rating : Really good service<br/>Product : Good camera for the price."
+                },
+                new Feedback
+                {
+                    COUNT = 2,
+                    CUSTOMERCOMMENT =
+                        "Service rating : This is a test service reviewwwwwwww0000000000000000000001<br/>Product : This is a test product reviewwwwwwww0000000000000000000001",
+                    DATE = "08-Sep-2015",
+                    DESCRIPTION = "this is a test",
+                    FACEBOOKSHARELINK =
+                        "http://www.facebook.com/sharer.php?u=http%3A%2F%2Fwww.feefo.com%2FGB%2Fen%2Freviews%2FExample-Supplier-Ltd%2F%3Fid%3D13%26servicefeedbackid%3D7445479",
+                    FEEDBACKID = 7445479,
+                    FurtherCommentsThread = new FurtherCommentsThread()
+                    {
+                        POST = new List<Post>()
+                        {
+                            new Post
+                            {
+                                DATE = "08-Sep-2015",
+                                VENDORCOMMENT = "This is a reply comment through the UI.<br><br>Adrian"
+                            },
+                            new Post
+                            {
+                                CUSTOMERCOMMENT = "this is a re-rated review - PRODUCT",
+                                DATE = "08-Sep-2015",
+                                PRODUCTRATING = "++",
+                                SERVICERATING = "++"
+                            }
+                        }
+                    },
+                    HREVIEWDATE = new DateTime(2015, 09, 08, 14, 57, 50),
+                    LINK = "http://www.examplesupplier.com/product_info.php?products_id=Comment013OSCommerceOSCommerce",
+                    PRODUCTCODE = "Comment01",
+                    PRODUCTLATEST = "++",
+                    PRODUCTRATING = "W",
+                    READMOREURL =
+                        "http://www.feefo.com/GB/en/reviews/Example-Supplier-Ltd/?id=13&servicefeedbackid=7445479",
+                    SERVICELATEST = "++",
+                    SERVICERATING = "W",
+                    SHORTCUSTOMERCOMMENT =
+                        "Service rating : This is a test service reviewwwwwwww0000000000000000000001<br/>Product : This is a test product reviewwwwwwww0000000000000000000001",
+                    SHORTVENDORCOMMENT = "This is a reply comment through the UI.<br/><br/>Adrian",
+                    VENDORCOMMENT = "This is a reply comment through the UI.<br/><br/>Adrian"
+                },
+                new Feedback
+                {
+                    COUNT = 3,
+                    CUSTOMERCOMMENT =
+                        "Service rating : I had an excellent experience<br/>Product : Great! It's the best thing ever,",
+                    DATE = "08-Jul-2015",
+                    DESCRIPTION = "Falcon Mach V",
+                    FACEBOOKSHARELINK =
+                        "http://www.facebook.com/sharer.php?u=http%3A%2F%2Fwww.feefo.com%2FGB%2Fen%2Freviews%2FExample-Supplier-Ltd%2F%3Fid%3D13%26servicefeedbackid%3D7445404",
+                    FEEDBACKID = 7445404,
+                    FurtherCommentsThread = new FurtherCommentsThread
+                    {
+                        POST = new List<Post>()
+                        {
+                            new Post
+                            {
+                                DATE = "24-Jul-2015",
+                                VENDORCOMMENT = "Product :test<br><br>Adrian"
+                            },
+                            new Post
+                            {
+                                DATE = "27-Jul-2015",
+                                VENDORCOMMENT = "this is a test comment<br><br>Adrian"
+                            },
+                            new Post
+                            {
+                                DATE = "27-Jul-2015",
+                                VENDORCOMMENT = "Product :this is a test for the second time.<br><br>Adrian"
+                            }
+                        }
+                    },
+                    HREVIEWDATE = new DateTime(2015, 07, 08, 21, 57, 22),
+                    HREVIEWRATING = 5,
+                    LINK = "http://www.examplesupplier.com/product_info.php?products_id=1323OSCommerceOSCommerce",
+                    PRODUCTCODE = 132,
+                    PRODUCTRATING = "++",
+                    READMOREURL =
+                        "http://www.feefo.com/GB/en/reviews/Example-Supplier-Ltd/?id=13&servicefeedbackid=7445404",
+                    SERVICERATING = "++",
+                    SHORTCUSTOMERCOMMENT =
+                        "Service rating : I had an excellent experience<br/>Product : Great! It's the best thing ever,",
+                    SHORTVENDORCOMMENT =
+                        "test<br/><br/>Adrian<br/><br/>this is a test comment<br/><br/>Adrian<br/><br/>this is a test for the second time.<br/><br/>Adrian",
+                    VENDORCOMMENT =
+                        "test<br/><br/>Adrian<br/><br/>this is a test comment<br/><br/>Adrian<br/><br/>this is a test for the second time.<br/><br/>Adrian"
+                },
+                new Feedback
+                {
+                    COUNT = 4,
+                    CUSTOMERCOMMENT = "As Above",
+                    DATE = "17-Apr-2015",
+                    DESCRIPTION = "Test",
+                    FACEBOOKSHARELINK =
+                        "http://www.facebook.com/sharer.php?u=http%3A%2F%2Fwww.feefo.com%2FGB%2Fen%2Freviews%2FExample-Supplier-Ltd%2F%3Fid%3D13%26servicefeedbackid%3D7445167",
+                    FEEDBACKID = 7445167,
+                    FurtherCommentsThread = new FurtherCommentsThread
+                    {
+                        POST = new List<Post>
+                        {
+                            new Post
+                            {
+                                DATE = "17-Apr-2015",
+                                VENDORCOMMENT = "Test<br><br>Adrian"
+                            }
+                        }
+                    },
+                    HREVIEWDATE = new DateTime(2015, 04, 17, 09, 52, 13),
+                    HREVIEWRATING = 5,
+                    LINK = "http://www.examplesupplier.com/product_info.php?products_id=Test3OSCommerceOSCommerce",
+                    PRODUCTCODE = "Test",
+                    PRODUCTRATING = "++",
+                    READMOREURL =
+                        "http://www.feefo.com/GB/en/reviews/Example-Supplier-Ltd/?id=13&servicefeedbackid=7445167",
+                    SERVICERATING = "++",
+                    SHORTCUSTOMERCOMMENT = "As Above",
+                    SHORTVENDORCOMMENT = "Test<br/><br/>Adrian",
+                    VENDORCOMMENT = "Test<br/><br/>Adrian"
+                }
+            };
         }
     }
 }
